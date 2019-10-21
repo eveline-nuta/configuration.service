cc40726f8ba8b283c9bfc82194520ee06319f412
Eveline Debora Nuta <eveline.nuta@makingwaves.com>
2019-10-21 16:18:38 +0200

    added structure of proj

---
+++ CofigurationApi/Controllers/ConfigurationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CofigurationApi.Controllers
+{
+    [ApiController]
+    [Route("[configuration]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly ILogger<ConfigurationController> _logger;
+        private readonly ConfigurationService _configurationService;
+
+        public ConfigurationController(ILogger<ConfigurationController> logger, ConfigurationService configurationService)
+        {
+            _logger = logger;
+            _configurationService = configurationService;
+        }
+
+        [HttpGet]
+        [Route("[get/{name}/{version}]")]
+        public string Get([FromBody] string name, string version)
+        {
+            var result=_configurationService.GetConfiguration(name, version);
+            return result;
+        }
+     
+    }
+}
---
+++ CofigurationApi/Service/ConfigurationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ConfigurationService
+{
+	public ConfigurationService()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public string GetConfiguration(string name, string version)
+    {
+        return "use function to access blobstorage";
+    }
+
+}
